9170e5518ec7f0c3924524419f6509835242edb4
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-27 20:41:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,12 +9,14 @@ class App < Sinatra::Base
   get '/reversename/:name' do 
     @name =params[:name].reverse
     @name
+    erb :reversename
   end
   
   get '/square/:number' do
     @number = params[:number].to_i
     @number = (@number*@number)
     @number
+    erb :square
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
