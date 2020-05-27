e6ceb396a79b5bf6178462d658a0415bee363fb7
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-27 20:37:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,13 +14,13 @@ class App < Sinatra::Base
     @number
   end
   
-  get '/say/:word1/:word2/:word3/:word4/:word5' do 
-    @word1=params[:word1]
-    @word2=params[:word2]
-    @word3=params[:word3]
-    @word4=params[:word4]
-    @word5=params[:word5]
-    <%="#{@word1}#{@word2}#{@word3}#{@word4}#{@word5}"%>
-  end 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+		@word1 = params[:word1]
+		@word2 = params[:word2]
+		@word3 = params[:word3]
+		@word4 = params[:word4]
+		@word5 = params[:word5]
+		"#{word1} #{word2} #{word3} #{word4} #{word5}".
+  end
   
 end
