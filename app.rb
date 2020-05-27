597d8b28df6b8d60dcdbc3b89d5d4ff9661f723b
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-27 20:30:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ class App < Sinatra::Base
   end
   
   get '/square/:number' do
-    <%@number = params[:number].to_i%>
-    <%@number = sqrt(@number)%>
+    @number = params[:number].to_i
+    @number = sqrt(@number)
     @number
   end
   
