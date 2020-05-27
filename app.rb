73a8f98d1dc71c23926d063ecc1e1eca763d8c28
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-27 20:50:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,6 +17,16 @@ class App < Sinatra::Base
 		(@number * @number).to_s
 	end
 
+  get '/say/:number/:phrase' do
+		@number = params[:number].to_i
+		@phrase = params[:phrase]
+		final_string = ""
+		@number.times do
+			final_string += @phrase
+		end
+		final_string
+	end
+
   get '/say/:word1/:word2/:word3/:word4/:word5' do
 		word1 = params[:word1]
 		word2 = params[:word2]
